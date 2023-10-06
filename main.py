efcd384c9d6c4d83dfb129c290cdabc70271066b
natasha1202 <chibi-natachka@yandex.ru>
2023-10-06 20:52:30 +0300

    first_get_request

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-#print('Hello, world!')
+import requests
+
+# print('Hello, world!')
 
 print('Hello from Natalia')
+
+# response = requests.get("https://playground.learnqa.ru/api/hello")
+# print(response.text)
+
+response = requests.get("https://playground.learnqa.ru/api/get_text")
+print(response.text)
