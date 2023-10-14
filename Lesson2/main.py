dbed61ed319267c1ea9dd1c4557c57e067fbe77d
natasha1202 <chibi-natachka@yandex.ru>
2023-10-15 02:15:02 +0300

    Lesson2_6

--- Lesson2/main.py
+++ Lesson2/main.py
@@ -1,5 +1,20 @@
+from json.decoder import JSONDecodeError
 import requests
 
-payload = {"name": "User"}
-response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+# payload = {"name": "User"}
+# response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+# print(response.text)
+
+response = requests.get("https://playground.learnqa.ru/api/hello", params = {"name": "User"})
+parsed_response_text = response.json()
+print(parsed_response_text["answer"])
+
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
+
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text)
+except JSONDecodeError:
+    print("Response is not a JSON format")
+
