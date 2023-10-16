4ffa0d03a55a640d4d57fb7099533630ef80a227
natasha1202 <chibi-natachka@yandex.ru>
2023-10-16 16:40:12 +0300

    HW_exercise7_part4

--- Lesson2/ex7_requests_and_methods_all_combinations.py
+++ Lesson2/ex7_requests_and_methods_all_combinations.py
@@ -1,24 +1,24 @@
 import requests
 
-methods = {"method": ["POST", "GET", "PUT", "DELETE"]}
+method_list = {"POST", "GET", "PUT", "DELETE"}
 
 # part4. all possible pairs method-request
 
-response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params="GET")
-print(response.text)
-response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data="POST")
-print(response.text)
-response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data="PUT")
-print(response.text)
-response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data="DELETE")
-print(response.text)
-
-for method in methods:
-        response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=method)
-        print(f'{method}, response text is {response.text}')
-        response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
-        print(f'{method}, response text is {response.text}')
-        response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
-        print(f'{method}, response text is {response.text}')
-        response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
-        print(f'{method}, response text is {response.text}')
+for method in method_list:
+        payload = {"method": method}
+        response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=payload)
+        print(f'GET-request, method = {method}')
+        print(response.status_code)
+        print(response.text)
+        response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
+        print(f'POST-request, method = {method}')
+        print(response.status_code)
+        print(response.text)
+        response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
+        print(f'PUT-request, method = {method}')
+        print(response.status_code)
+        print(response.text)
+        response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
+        print(f'DELETE-request, method = {method}')
+        print(response.status_code)
+        print(response.text)
