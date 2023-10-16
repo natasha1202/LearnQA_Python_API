159adcb83430d3d2a3a1d8bb7c237010da0375f7
natasha1202 <chibi-natachka@yandex.ru>
2023-10-16 23:25:50 +0300

    HW_exercise7 new function

--- Lesson2/ex7_requests_and_methods_correct_pair.py
+++ Lesson2/ex7_requests_and_methods_correct_pair.py
@@ -2,26 +2,25 @@ import requests
 
 method_list = {"POST", "GET", "PUT", "DELETE"}
 
+
+def info_message(response):
+    print(method)
+    print(response.text)
+    print(response.status_code)
+
+
 # part3. Correct pair method-request
 for method in method_list:
     payload = {"method": method}
     if method == "GET":
         response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=payload)
-        print(method)
-        print(response.text)
-        print(response.status_code)
+        info_message(response)
     elif method == "POST":
         response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
-        print(method)
-        print(response.text)
-        print(response.status_code)
+        info_message(response)
     elif method == "PUT":
         response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
-        print(method)
-        print(response.text)
-        print(response.status_code)
+        info_message(response)
     elif method == "DELETE":
         response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
-        print(method)
-        print(response.text)
-        print(response.status_code)
+        info_message(response)
