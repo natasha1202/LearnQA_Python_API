587bdaf16b7df85c338d5b444cde6d1f03be9d6b
natasha1202 <chibi-natachka@yandex.ru>
2023-10-16 23:33:17 +0300

    HW_exercise7 new function

--- Lesson2/ex7_requests_and_methods_all_combinations.py
+++ Lesson2/ex7_requests_and_methods_all_combinations.py
@@ -2,23 +2,25 @@ import requests
 
 method_list = {"POST", "GET", "PUT", "DELETE"}
 
+
+def info_message(response):
+    print(response.status_code)
+    print(response.text)
+
+
 # part4. all possible pairs method-request
 
 for method in method_list:
-        payload = {"method": method}
-        response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=payload)
-        print(f'GET-request, method = {method}')
-        print(response.status_code)
-        print(response.text)
-        response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
-        print(f'POST-request, method = {method}')
-        print(response.status_code)
-        print(response.text)
-        response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
-        print(f'PUT-request, method = {method}')
-        print(response.status_code)
-        print(response.text)
-        response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
-        print(f'DELETE-request, method = {method}')
-        print(response.status_code)
-        print(response.text)
+    payload = {"method": method}
+    response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=payload)
+    print(f'GET-request, method = {method}')
+    info_message(response)
+    response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
+    print(f'POST-request, method = {method}')
+    info_message(response)
+    response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
+    print(f'PUT-request, method = {method}')
+    info_message(response)
+    response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
+    print(f'DELETE-request, method = {method}')
+    info_message(response)
