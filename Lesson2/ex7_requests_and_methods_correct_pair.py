ef3d8e3d45f0276489b37f3642db46886b52dfe7
natasha1202 <chibi-natachka@yandex.ru>
2023-10-16 16:29:00 +0300

    HW_exercise7_part3_2

--- Lesson2/ex7_requests_and_methods_correct_pair.py
+++ Lesson2/ex7_requests_and_methods_correct_pair.py
@@ -1,23 +1,27 @@
 import requests
 
-method_list = ["POST", "GET", "PUT", "DELETE"]
-payload = {"method": method_list}
+method_list = {"POST", "GET", "PUT", "DELETE"}
 
 # part3. Correct pair method-request
 for method in method_list:
+    payload = {"method": method}
     if method == "GET":
         response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=payload)
+        print(method)
         print(response.text)
         print(response.status_code)
     elif method == "POST":
         response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
+        print(method)
         print(response.text)
         print(response.status_code)
     elif method == "PUT":
         response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
+        print(method)
         print(response.text)
         print(response.status_code)
     elif method == "DELETE":
         response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payload)
+        print(method)
         print(response.text)
         print(response.status_code)
