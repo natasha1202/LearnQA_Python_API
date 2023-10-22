a2af233e4d86ba1fbae90836ae5b0e12b8038500
natasha1202 <chibi-natachka@yandex.ru>
2023-10-23 01:02:57 +0300

    HW_exercise11 method cookie

--- Lesson3/main.py
+++ Lesson3/main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(response.cookies)
+print(response.text)
+print(response.headers)
+print(response.cookies.get("HomeWork"))
---
+++ Lesson3/test_method_cookie.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+class TestMethodCookie:
+    def test_method_cookie(self):
+        response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
+        cookie = response.cookies
+        print(cookie)
+        expected_cookie = "<RequestsCookieJar[<Cookie HomeWork=hw_value for .playground.learnqa.ru/>]>"
+        assert expected_cookie == cookie, "Cookie HomeWork is different from the expected one"
+
+# <RequestsCookieJar[Cookie(version=0, name='HomeWork', value='hw_value', port=None, port_specified=False, domain='.play...ure=False, expires=1700613421, discard=False, comment=None, comment_url=None, rest={'HttpOnly': None}, rfc2109=False)]>
+
