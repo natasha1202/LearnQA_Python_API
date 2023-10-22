051b75cc4f75ec113ca38bf3b49aeeeef78adf8b
natasha1202 <chibi-natachka@yandex.ru>
2023-10-23 01:34:45 +0300

    HW_exercise12 headers

---
+++ Lesson3/test_method_headers.py
@@ -0,0 +1,9 @@
+import requests
+
+class TestMethodHeaders:
+
+    def test_method_headers(self):
+        response = requests.get("https://playground.learnqa.ru/api/homework_header")
+        header = response.headers.get("x-secret-homework-header")
+        print(response.headers)
+        assert "Some secret value" == header, "The header is different from the expected one"
