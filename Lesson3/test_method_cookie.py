f99642134973b4cdf67e1ed9c558a450c99a3939
natasha1202 <chibi-natachka@yandex.ru>
2023-10-23 01:05:02 +0300

    HW_exercise11 errormessage corrected

--- Lesson3/test_method_cookie.py
+++ Lesson3/test_method_cookie.py
@@ -7,7 +7,7 @@ class TestMethodCookie:
         cookie = response.cookies
         print(cookie)
         expected_cookie = "<RequestsCookieJar[<Cookie HomeWork=hw_value for .playground.learnqa.ru/>]>"
-        assert expected_cookie == cookie, "Cookie HomeWork is different from the expected one"
+        assert expected_cookie == cookie, "The cookie is different from the expected one"
 
 # <RequestsCookieJar[Cookie(version=0, name='HomeWork', value='hw_value', port=None, port_specified=False, domain='.play...ure=False, expires=1700613421, discard=False, comment=None, comment_url=None, rest={'HttpOnly': None}, rfc2109=False)]>
 
