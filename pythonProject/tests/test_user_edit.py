22f051a2967462038b3a677c9fe115ec2d78baf2
natasha1202 <chibi-natachka@yandex.ru>
2023-11-04 18:23:57 +0300

    Lesson4_6 method replacement

--- pythonProject/tests/test_user_edit.py
+++ pythonProject/tests/test_user_edit.py
@@ -1,4 +1,4 @@
-import requests
+from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
@@ -6,7 +6,7 @@ class TestUserEdit(BaseCase):
     def test_edit_just_created_user(self):
         # REGISTER
         register_data = self.prepare_registration_data()
-        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
+        response1 = MyRequests.post("/user/", data=register_data)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
@@ -22,7 +22,7 @@ class TestUserEdit(BaseCase):
             'password': password
         }
 
-        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+        response2 = MyRequests.post("/user/login", data=login_data)
 
         auth_sid = self.get_cookie(response2, "auth_sid")
         token = self.get_header(response2, "x-csrf-token")
@@ -30,8 +30,8 @@ class TestUserEdit(BaseCase):
         # EDIT
         new_name = "Changed name"
 
-        response3 = requests.put(
-            f"https://playground.learnqa.ru/api/user/{user_id}",
+        response3 = MyRequests.put(
+            f"/user/{user_id}",
             headers={"x-csrf-token": token},
             cookies={"auth_sid": auth_sid},
             data={"firstName": new_name}
@@ -41,8 +41,8 @@ class TestUserEdit(BaseCase):
 
         # GET
 
-        response4 = requests.get(
-            f"https://playground.learnqa.ru/api/user/{user_id}",
+        response4 = MyRequests.get(
+            f"/user/{user_id}",
             headers={"x-csrf-token": token},
             cookies={"auth_sid": auth_sid}
         )
--- pythonProject/tests/test_user_register.py
+++ pythonProject/tests/test_user_register.py
@@ -1,4 +1,4 @@
-import requests
+from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
@@ -6,24 +6,17 @@ class TestUserRegister(BaseCase):
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
-        # assert response.status_code == 200, f"Unexpected status code {response.status_code}"
-        # print(response.content)
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
-        # print(response.content)
 
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
         data = self.prepare_registration_data(email)
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
-        print(response.status_code)
-        print(response.content)
-
-        # assert response.status_code == 400, f"Unexpected status code {response.status_code}"
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", (f"Unexpected status content "
                                                                                   f"{response.content}")
