ca6d3dd11346462d6f6204e9cbcaae944db26fb4
natasha1202 <chibi-natachka@yandex.ru>
2023-11-04 16:52:56 +0300

    Lesson4_4 User edit

--- pythonProject/tests/test_user_register_old.py
+++ pythonProject/tests/test_user_register_old.py
@@ -1,10 +1,25 @@
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from datetime import datetime
 
-class TestUserRegister(BaseCase):
+
+class TestUserRegisterOld(BaseCase):
+
+    def setup_method(self):
+        base_part = "learnqa"
+        domain = "example.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{base_part}{random_part}@{domain}"
+
     def test_create_user_successfully(self):
-        data = self.prepare_registration_data()
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': self.email
+        }
 
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
@@ -12,11 +27,16 @@ class TestUserRegister(BaseCase):
         # print(response.content)
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
-        # print(response.content)
 
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
-        data = self.prepare_registration_data(email)
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
 
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
@@ -25,7 +45,6 @@ class TestUserRegister(BaseCase):
 
         # assert response.status_code == 400, f"Unexpected status code {response.status_code}"
         Assertions.assert_code_status(response, 400)
-        assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", (f"Unexpected status content "
-                                                                                  f"{response.content}")
-
-
+        assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", (
+            f"Unexpected status content "
+            f"{response.content}")
